289baccaa9be79ca116268d6cb8bd416767c8c25
constantyn <constantyn@gmail.com>
2024-02-13 16:11:13 +0100

    Add Vuetify

--- app.vue
+++ app.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <v-app>
+    <v-layout>
+      <v-app-bar title="Opera Sessions">
+        <template v-slot:append>
+          <v-app-bar-nav-icon @click="drawerOpen = !drawerOpen" />
+        </template>
+      </v-app-bar>
+
+      <v-navigation-drawer v-model="drawerOpen" location="end">
+        <v-list>
+          <v-list-item title="Home"/>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main>
+        <v-sheet class="ma-6">
+          <v-alert type="info">Hello world!</v-alert>
+        </v-sheet>
+      </v-main>
+    </v-layout>
+  </v-app>
 </template>
+
+<script lang="ts" setup>
+const drawerOpen = ref(false);
+</script>
